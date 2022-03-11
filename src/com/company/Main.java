999d857205928819fdf3f7adf3e3418aeb80c210
Rosilaynne Rosa <rosilaynne@gmail.com>
2022-03-10 22:11:25 -0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("• Nome: Rosilaynne Rosa \n" + "• Idade: 31 \n"
+        + "• Formação: Engenharia Elétrica de Telecomunicações \n"
+        + "• Estou estudando: Full-Stcak \n" + "• LinkedIn: https://www.linkedin.com/in/rosilaynne-rosa/ \n"
+        + "• Expectativa: Conseguir um emprego de Desenvolvedor ");
+        
+    }
+}
